989999035f8923dd8aa8766127d686702af4bbb4
DaniloIS <dailosantos-pg@hotmail.com>
2022-03-15 14:39:45 -0300

    Componente Media, Soma, Sorteio e implementando useState

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -5,16 +5,19 @@ import Card from './Card';
 import './Intervalo.css'
 
 export default props => {
+
+    const { min, max } = props;
+
     return (
         <Card title='Intervalo de Números' red>
             <div className='Intervalo'>
                 <span>
                     <strong>Mínimo:</strong>
-                    <input type="number" value={0} readOnly />
+                    <input type="number" value={min} onChange={e => props.onMinChange(e.target.value)} />
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type="number" value={10} readOnly />
+                    <input type="number" value={max} onChange={e => props.onMaxChange(e.target.value)} />
                 </span>
             </div>
         </Card>
